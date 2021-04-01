ba59026cfc6833cf2d8e1d3fd0818a3510ea8127
sergenorhann <sergenorhann@users.noreply.github.com>
2021-04-01 15:52:11 +0300

    01.04.2021

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -44,6 +44,8 @@ namespace WebAPI
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
             });
 
+
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
